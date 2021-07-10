e2574a942f800184579377e707f6008838a0c070
sngina <sngina707@gmail.com>
2021-07-10 11:50:28 +0300

    add class image in models.py

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.shortcuts import render , redirect
 
 # Create your models here.
+class Image(models.Model):
+    image_photo = models.ImageField(upload_to = 'image /' , null = True)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=30)
+
+    # the save function
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+    #delete function for image
+    @classmethod
+    def delete_image(cls ,id):
+        cls.objects.filter(id = id).delete()
+    
+    #updating caption
+    @classmethod
+    def update(cls , id , update) :
+        caption = cls.objects.filter(id = id).update(image_caption = update)
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import IncludedURLConf
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns = [
+    url('^$' , views.welcome, name = 'welcome'),
+]
