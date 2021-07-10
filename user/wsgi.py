04b5b2f9a0b308c5d40905208e4b1bc5eb135a42
sngina <sngina707@gmail.com>
2021-07-10 10:34:47 +0300

    pip  install wheel

--- user/wsgi.py
+++ user/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for instagram project.
+WSGI config for user project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instagram.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user.settings")
 
 application = get_wsgi_application()
